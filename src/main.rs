a28ab9b85d05db36bf40fb6c6416022ca43e9561
Valkyrie-freja <yadokame3.14@gmail.com>
2022-07-22 08:51:14 +0900

    end.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use std::io;
 
 fn main() {
+    //println!("input string > ");
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("You input: {}", input);
-    
+    io::stdin().read_line(&mut input).unwrap()/*.expect("Failed to read line")*/;
+    //println!("You input: {}", input);
+    let chars: Vec<char> = input.chars().collect();
+    let mut has_dbl:[bool; 3] = [false; 3];
+    for i in 0..3 {
+        for j in i..3-i {
+            if i == j { continue; }
+            if chars[i] == chars[j] {
+                has_dbl[i] = true;
+                has_dbl[j] = true;
+            }
+        }
+    }
+
+    for i in 0..3{
+        if has_dbl[i] == false {
+            println!("{}", chars[i]);
+            return;
+        }
+    }
+    println!("-1");
 }
